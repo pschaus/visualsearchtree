43ad2cbe2a07d3a66a6cc78c22efbb7b9c330543
Genereux-akotenou <genereux.akotenou@gmail.com>
2022-07-13 16:51:51 +0100

    All constants value defined in a file

---
+++ src/main/java/org/uclouvain/visualsearchtree/util/Constant.java
@@ -0,0 +1,9 @@
+package org.uclouvain.visualsearchtree.util;
+
+public class Constant {
+    public static final double ZOOM_COEFFICIENT = 1.5;
+    public static final double SCALE_COEFFICIENT = 0.01;
+    public static final int LABEL_X_COEFFICIENT = 40;
+    public static final int LABEL_Y_COEFFICIENT = 50;
+
+}
