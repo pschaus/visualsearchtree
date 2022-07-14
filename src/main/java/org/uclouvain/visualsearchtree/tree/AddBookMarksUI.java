745cdedbd678a39d59643806febc1fead130f73c
Genereux-akotenou <genereux.akotenou@gmail.com>
2022-07-14 12:34:16 +0100

    Fixed: new value for bookmark key to assure the unicity Feat: Update table view automatically when bookmark tabPane is selectionned

--- src/main/java/org/uclouvain/visualsearchtree/tree/AddBookMarksUI.java
+++ src/main/java/org/uclouvain/visualsearchtree/tree/AddBookMarksUI.java
@@ -4,6 +4,8 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyCode;
+import javafx.scene.robot.Robot;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -21,14 +23,20 @@ public class AddBookMarksUI {
 
     public void addBookMarksToNode(ActionEvent actionEvent){
         String bookMark = nodeBookmark.getText();
+        var focusedNode = instance.getFocusedRect();
+        String nodeLabel = ((Text) focusedNode.get(2)).getText();
         if(bookMark.trim().equals("")){
             displayAlertError("The BookMark value can not be null");
         }else{
-            String key = ( (Text) instance.getFocusedRect().get(2) ).getText();
+            //key is made by concatening "node", nodeId and nodeLabel
+            String key = "node"+focusedNode.get(3)+" "+nodeLabel;
             instance.setBoookMarks(key,bookMark);
-            Rectangle r = (Rectangle) instance.getFocusedRect().get(0);
+            Rectangle r = (Rectangle) focusedNode.get(0);
             r.setStrokeWidth(3);
             closeWindow();
+            // Update the bookmarks tableview by simulating pressing the B key
+            Robot robot = new Robot();
+            robot.keyPress(KeyCode.B);
         }
     }
 
