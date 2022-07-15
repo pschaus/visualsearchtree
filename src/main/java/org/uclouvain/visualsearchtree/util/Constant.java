e9c28c3964079c47f83771977ebf6e0c44576430
Genereux-akotenou <genereux.akotenou@gmail.com>
2022-07-15 09:34:04 +0100

    Feat: New constants added

--- src/main/java/org/uclouvain/visualsearchtree/util/Constant.java
+++ src/main/java/org/uclouvain/visualsearchtree/util/Constant.java
@@ -5,5 +5,9 @@ public class Constant {
     public static final double SCALE_COEFFICIENT = 0.01;
     public static final int LABEL_X_COEFFICIENT = 40;
     public static final int LABEL_Y_COEFFICIENT = 50;
+    public static final int NODE_SHAPE_SIZE = 20;
+    public static final int NODE_SHAPE_ARC_VALUE = NODE_SHAPE_SIZE*2;
+    public static final int LEGEND_SHAPE_SIZE = 12;
+    public static final int LEGEND_SHAPE_ARC_VALUE = LEGEND_SHAPE_SIZE*2;
 
 }
