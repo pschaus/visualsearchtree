04508bda8aa84778c87e8e988009034eb7358bd1
Genereux-akotenou <genereux.akotenou@gmail.com>
2022-08-03 10:49:36 +0100

    Feat: Event on double or right click for calling nodeAction Feat : nodeAction override method usable Feat: For Nqueens display a static chess for Node visualisation Fixed : Some methods comments added

--- src/main/java/org/uclouvain/visualsearchtree/tree/TreeVisual.java
+++ src/main/java/org/uclouvain/visualsearchtree/tree/TreeVisual.java
@@ -8,11 +8,14 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Cursor;
 import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.chart.XYChart.Data;
+import javafx.scene.input.MouseButton;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
@@ -20,6 +23,8 @@ import javafx.scene.shape.*;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 import org.uclouvain.visualsearchtree.tree.events.BackToNormalEvent;
 import org.uclouvain.visualsearchtree.tree.events.BackToNormalEventHandler;
@@ -32,23 +37,23 @@ import static org.uclouvain.visualsearchtree.util.Constant.*;
 public class TreeVisual {
     private Tree.Node<String> node;
     private List<Integer> legendStats;
-    private List<Text> labels;
+    private final List<Text> labels;
     private String info;
-    private List focusedRect;
+    private final List focusedRect;
     private  StackPane treeStackPane;
-    private Map<String,String> boookMarks;
+    private final Map<String,String> boookMarks;
     private Map<String, Rectangle> allNodesRects;
     private Map<String, XYChart.Data> allNodesChartDatas;
     private Map<String, Tree.PositionedNode<String>> allNodesPositions;
     private List<Tree.Node> tempList;
     private  Tree tree;
-    ///
+
     NumberAxis xAxis = new NumberAxis();
     NumberAxis yAxis = new NumberAxis();
 
     private LineChart lineChart;
     private XYChart.Series series;
-    private HBox legendhbox;
+    private HBox legendbox;
 
     public TreeVisual(Tree.Node<String> node) {
         this.node = node;
@@ -80,7 +85,7 @@ public class TreeVisual {
         this.boookMarks = new HashMap<String,String>();
         tree = new Tree(-1);
         this.node = tree.root();
-        this.legendhbox = new HBox();
+        this.legendbox = new HBox();
 
         this.xAxis = new NumberAxis();
         this.yAxis = new NumberAxis();
@@ -171,18 +176,30 @@ public class TreeVisual {
         return root;
     }
 
-
+    /**
+     * Draw Node recursively
+     * @param g group to add node
+     * @param root root node
+     * @param center double value for centering the tree
+     * @param depth depth of the tree
+     * @param nLabel Text widget for containing the node Label
+     * @return Rectangle which can be circle or losange representing node and depending of its type
+     */
     public  Rectangle drawNodeRecur(Group g, Tree.PositionedNode<String> root, double center, int depth, Text nLabel) {
         double absolute = center + root.position;
-        Gson gz = new Gson();
-        NodeInfoData info = null;
+        Gson gson = new Gson();
+        NodeInfoData info= null;
 
         Rectangle r = createRectangle(400 + absolute * 40, 50 + depth * 50, root.type);
         styleLabel(nLabel, absolute, depth, root.label, root.position, root.children.size());
 
         //Add Event to each rectangle
+        root.nodeAction = (nodeInfoData) -> drawNewVisualisation(nodeInfoData);
         r.setOnMouseClicked(e -> {
-            //root.nodeAction();
+            if (e.getClickCount() == 2 || e.getButton()== MouseButton.SECONDARY) {
+                NodeInfoData infoData = gson.fromJson(root.info, new TypeToken<TreeVisual.NodeInfoData>(){}.getType());
+                root.nodeAction.nodeAction(infoData);
+            }
             r.fireEvent(new BackToNormalEvent());
             r.setFill(Color.ORANGE);
             nLabel.setOpacity((nLabel.getOpacity())==1? 0:1);
@@ -207,8 +224,7 @@ public class TreeVisual {
         if (depth > this.legendStats.get(3)) {
             this.setLegendStats(3, depth);
         }
-
-        info = gz.fromJson(root.info, new TypeToken<NodeInfoData>(){}.getType());
+        info = gson.fromJson(root.info, new TypeToken<NodeInfoData>(){}.getType());
         if (info != null) {
             String nodeID = UUID.randomUUID().toString();
             this.allNodesPositions.put(nodeID, root);
@@ -217,7 +233,45 @@ public class TreeVisual {
         return r;
     }
 
+    /**
+     * Draw a visualisation : Here a chess with fixed value of node is drawn
+     * @param nodeInfoData info parse to gson object of the concerned node
+     */
+    private void drawNewVisualisation(NodeInfoData nodeInfoData) {
+        int nRow = 4, nCol=4;
+        GridPane chess = new GridPane();
+        chess.setHgap(2);
+        chess.setVgap(2);
+        Scene chessScene = new Scene(chess, nRow*50, nCol*50);
+        Stage chessWindow = new Stage();
 
+        chessWindow.setTitle("Nqueens Visualisation Board");
+        chessWindow.setScene(chessScene);
+
+        for(int i=0;i<nRow;i++){
+            for(int j=0;j<nCol;j++){
+                if((i==0 && j==1) || (i==1 && j==3) || (i==2 && j==0) || (i==3 && j==2)){
+                    chess.add(createRectangleForBoard(true), j, i);
+                }else{
+                    chess.add(createRectangleForBoard(false), j, i);
+                }
+            }
+        }
+        chessWindow.initModality(Modality.WINDOW_MODAL);
+        chessWindow.initOwner(VisualTree.pStage);
+        chessWindow.setX(VisualTree.pStage.getX());
+        chessWindow.setY(VisualTree.pStage.getY());
+
+        chessWindow.show();
+        System.out.println(nodeInfoData);
+    }
+
+    /**
+     * @param x width of the rectangle
+     * @param y height of the rectangle
+     * @param type Node Type
+     * @return Rectangle representing the node
+     */
     private Rectangle createRectangle(double x, double y, Tree.NodeType type) {
         Rectangle rect = new Rectangle(x,y,NODE_SHAPE_SIZE,NODE_SHAPE_SIZE);
         rect.setStrokeType(StrokeType.OUTSIDE);
@@ -253,6 +307,11 @@ public class TreeVisual {
         return rect;
     }
 
+    /**
+     * Draw Rectangle for Legend HBox
+     * @param type Node Type
+     * @return Rectangle representing a type of node
+     */
     private Rectangle createRectangleForLegendBox(Tree.NodeType type) {
         Rectangle rect = new Rectangle();
         rect.setWidth(LEGEND_SHAPE_SIZE);
@@ -278,6 +337,26 @@ public class TreeVisual {
         return rect;
     }
 
+    /**
+     * Draw Rectangle for Chess Visualisation for Nqueens problem
+     * @param isFixed boolean which indicate if a variable has its value fixed
+     * @return Rectangle which represent a case
+     */
+    private Rectangle createRectangleForBoard(boolean isFixed){
+        Rectangle r = new Rectangle(50,50);
+        r.setFill(isFixed ? Color.BLACK : Color.WHITE);
+        return r;
+    }
+
+    /**
+     * Style Each node Label by setting its content and positioning it to a suitable position on the scene
+     * @param theLabel Text Widget
+     * @param absolute absolute position value
+     * @param depth tree depth
+     * @param content Node label to show
+     * @param pos position value
+     * @param nChild number of children the current node has
+     */
     private void styleLabel(Text theLabel, double absolute , double depth, String content, double pos, int nChild) {
 
         theLabel.setFont(Font.font("Roboto", 10));
@@ -310,6 +389,12 @@ public class TreeVisual {
 
     }
 
+    /**
+     * Create a line for connecting two Nodes represented by a rectangle
+     * @param r1 first rectangle
+     * @param r2 second rectangle
+     * @return Line for connecting rectangles
+     */
     private Line connectRectangle(Rectangle r1, Rectangle r2) {
         Line line = new Line();
         line.setStartX(r1.getX()+r1.getWidth()/2);
@@ -323,9 +408,13 @@ public class TreeVisual {
         return line;
     }
 
+    /**
+     * Generate Legend Box for specify number of each type of nodes
+     * @return HBox
+     */
     public HBox generateLegendsStack(){
-        legendhbox.setPadding(new Insets(10));
-        legendhbox.setAlignment(Pos.BASELINE_LEFT);
+        legendbox.setPadding(new Insets(10));
+        legendbox.setAlignment(Pos.BASELINE_LEFT);
         Rectangle branchRect = createRectangleForLegendBox(Tree.NodeType.INNER);
         Rectangle solvedRect = createRectangleForLegendBox(Tree.NodeType.SOLUTION);
         Rectangle failedRect = createRectangleForLegendBox(Tree.NodeType.FAIL);
@@ -335,11 +424,13 @@ public class TreeVisual {
         s1.getChildren().addAll(branchRect, new Text("  ("+ this.legendStats.get(0)+")"));
         s2.getChildren().addAll(failedRect, new Text("  ("+ this.legendStats.get(1)+")"));
         s3.getChildren().addAll(solvedRect, new Text("  ("+ this.legendStats.get(2)+")"));
-        legendhbox.getChildren().addAll(s1,s2,s3,new  Text("DEPTH : ("+ this.legendStats.get(3)+")"));
-        return legendhbox;
+        legendbox.getChildren().addAll(s1,s2,s3,new  Text("DEPTH : ("+ this.legendStats.get(3)+")"));
+        return legendbox;
     }
 
-
+    /**
+     * Make the previous node selected to it initial state : No more focus color...
+     */
     public void makeNotFocus(){
         var r = (Rectangle) this.focusedRect.get(0);
         var branch = (Tree.NodeType) this.focusedRect.get(1);
@@ -359,8 +450,8 @@ public class TreeVisual {
 
     /**
      * Create the optimization chart
-     * @param all_sol
-     * @return
+     * @param all_sol boolean
+     * @return LineChart
      */
     public LineChart<Number, Number> getTreeChart(boolean all_sol){
         // variables
@@ -421,8 +512,7 @@ public class TreeVisual {
     /**
      * Use function to add event on chart
      */
-    public void addEventOnChart()
-    {
+    public void addEventOnChart() {
         for (String key: this.allNodesChartDatas.keySet())
         {
             if (this.allNodesChartDatas.get(key) != null)
@@ -510,9 +600,9 @@ public class TreeVisual {
         this.allNodesChartDatas = new Hashtable<>();
 
         //Empty legendBox
-        if (legendhbox.getChildren().size() >  0)
+        if (legendbox.getChildren().size() >  0)
         {
-            legendhbox.getChildren().remove(0, legendhbox.getChildren().size());
+            legendbox.getChildren().remove(0, legendbox.getChildren().size());
         }
         this.legendStats = new ArrayList<>(){{
             add(0);
