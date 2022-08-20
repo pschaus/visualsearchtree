70185ccecda4830b3d5d75de1abd2640a19c37bc
Afis KOUSSE <mahoutinafis@gmail.com>
2022-08-20 04:57:06 +0100

    Get NQueens Node Data for example

--- src/main/java/org/uclouvain/visualsearchtree/examples/NQueensPruneVisu.java
+++ src/main/java/org/uclouvain/visualsearchtree/examples/NQueensPruneVisu.java
@@ -16,6 +16,7 @@
 package org.uclouvain.visualsearchtree.examples;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
@@ -30,60 +31,55 @@ import java.util.concurrent.TimeUnit;
  * that backtracks as soon as a decision conflicts with
  * the previous decisions.
  */
-public class NQueensPruneVisu extends Application {
+public class NQueensPruneVisu {
 
     public static void main(String[] args) {
-        launch(args);
+
+        Platform.startup(() -> {
+            NQueensPrune nqueens = new NQueensPrune(7);
+            Tree t = new Tree(-1);
+            TreeVisual tv = new TreeVisual();
+
+            tv.setRealtimeItv(1000);
+            tv.setRealtimeNbNodeDrawer(5);
+            // TEST: TO SIMULATE OPTIMIZATION GRAPH
+            VisualTree.treeProfilerLaucher(tv);
+
+            Thread t2 = new Thread(() -> nqueens.dfs(new DFSListener() {
+                @Override
+                public void solution(int id, int pId) {
+                    System.out.println("solution");
+                    tv.createNode(id,pId, Tree.NodeType.SOLUTION,(nodeInfoData) -> {}, "{\"cost\": "+id+", \"param1\": "+id+", \"other\": \""+ getNodeValue(nqueens.q)+"\"}");
+                }
+                @Override
+                public void fail(int id, int pId) {
+                    System.out.println("fail");
+                    tv.createNode(id,pId, Tree.NodeType.FAIL,(nodeInfoData) -> {}, "{\"cost\": "+id+", \"param1\": "+id+", \"other\": \""+ getNodeValue(nqueens.q)+"\"}");
+                }
+                @Override
+                public void branch(int id, int pId, int nChilds) {
+                    System.out.println("branch");
+                    tv.createNode(id,pId, Tree.NodeType.INNER,(nodeInfoData) -> {}, "{\"cost\": "+id+", \"param1\": "+id+", \"other\": \""+ getNodeValue(nqueens.q)+"\"}");
+                }
+            }));
+            t2.start();
+        });
     }
 
-    @Override
-    public void start(Stage primaryStage) {
-        NQueensPrune nqueens = new NQueensPrune(10);
-        Tree t = new Tree(-1);
-        TreeVisual tv = new TreeVisual();
-
-        tv.setRealtimeItv(100);
-        tv.setRealtimeNbNodeDrawer(700);
-        // TEST: TO SIMULATE OPTIMIZATION GRAPH
-        VisualTree.treeProfilerLaucher(tv);
-
-        Thread t2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                nqueens.dfs(new DFSListener() {
-                    @Override
-                    public void solution(int id, int pId) {
-                        System.out.println("solution");
-                        tv.createNode(id,pId, Tree.NodeType.SOLUTION,(nodeInfoData) -> {}, "{\"cost\": "+id+", \"param1\": "+id+", \"other\": \"Some info on node\"}");
-                    }
-                    @Override
-                    public void fail(int id, int pId) {
-                        System.out.println("fail");
-                        tv.createNode(id,pId, Tree.NodeType.FAIL,(nodeInfoData) -> {}, "{\"cost\": "+id+", \"param1\": "+id+", \"other\": \"Some info on node\"}");
-                    }
-                    @Override
-                    public void branch(int id, int pId, int nChilds) {
-                        System.out.println("branch");
-                        tv.createNode(id,pId, Tree.NodeType.INNER,(nodeInfoData) -> {}, "{\"cost\": "+id+", \"param1\": "+id+", \"other\": \"Some info on node\"}");
-                    }
-                });
-            }
-        });
-        t2.start();
-
-        //Tree.Node n = t.root();
-
-        // Let use visualTree screen to visualize both
-        // tree, optimization graph, node info and bookmarks
-        //VisualTree.treeProfilerLauncher(n, primaryStage);
-
-        // StackPane p = new StackPane();
-        // AnimationFactory.zoomOnSCroll(p);
-        // TreeVisual tv = new TreeVisual(t.root());
-        // p.getChildren().add(tv.getGroup());
-        // Scene scene = new Scene(p, 500, 600);
-        // primaryStage.setScene(scene);
-        // primaryStage.show();
-
+    /**
+     * Smple function to return the node value as string during the search
+     * tab of "index" : value (0: q[0] | 1: q[1] |... n: q[n])
+     * @param q: int tab
+     * @return : String value
+     */
+    public static String getNodeValue(int[] q){
+        StringBuilder value = new StringBuilder("{");
+        for (int i = 0; i < q.length; i++) {
+            value.append(i).append(":").append(q[i]);
+            if (i != (q.length - 1))
+                value.append(",");
+        }
+        value.append("}");
+        return value.toString();
     }
 }
