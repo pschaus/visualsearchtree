c3612e93c5b36b7daf8794618b1e0be4c094a78e
Genereux-akotenou <genereux.akotenou@gmail.com>
2022-09-13 15:49:15 +0100

    Test: fixe javafx warning

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,15 +4,21 @@ module org.uclouvain.visualsearchtree {
     requires java.desktop;
     requires com.google.gson;
 
+//    opens org.uclouvain.visualsearchtree to javafx.fxml;
+//    opens org.uclouvain.visualsearchtree.tree to javafx.fxml;
+//    opens org.uclouvain.visualsearchtree.bridge to javafx.fxml;
+//    opens org.uclouvain.visualsearchtree.server to javafx.fxml;
+//    opens org.uclouvain.visualsearchtree.examples to javafx.fxml;
 
-    opens org.uclouvain.visualsearchtree to javafx.fxml;
+    opens org.uclouvain.visualsearchtree to javafx.graphics;
+    opens org.uclouvain.visualsearchtree.tree to javafx.graphics;
+    opens org.uclouvain.visualsearchtree.bridge to javafx.graphics;
+    opens org.uclouvain.visualsearchtree.server to javafx.graphics;
+    opens org.uclouvain.visualsearchtree.examples to javafx.graphics;
+
     exports org.uclouvain.visualsearchtree;
     exports org.uclouvain.visualsearchtree.tree;
-    opens org.uclouvain.visualsearchtree.tree to javafx.fxml;
     exports org.uclouvain.visualsearchtree.bridge;
-    opens org.uclouvain.visualsearchtree.bridge to javafx.fxml;
     exports org.uclouvain.visualsearchtree.server;
-    opens org.uclouvain.visualsearchtree.server to javafx.fxml;
-    opens org.uclouvain.visualsearchtree.examples to javafx.fxml;
     exports org.uclouvain.visualsearchtree.examples;
 }
