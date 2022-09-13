a0380fac2101e816ca9b76a29f11160a401e0ab7
Genereux-akotenou <genereux.akotenou@gmail.com>
2022-09-13 16:46:14 +0100

    Fixed : Some modifications about node representation

--- src/main/java/org/uclouvain/visualsearchtree/tree/Anchor.java
+++ src/main/java/org/uclouvain/visualsearchtree/tree/Anchor.java
@@ -19,6 +19,7 @@ public class Anchor extends Circle {
     DoubleProperty x, y;
     int nbChild;
     int depth;
+    NodeAction nodeAction;
     Anchor parent = null;
     List<Anchor> children = new ArrayList<>();
     List<DoubleProperty> levels = null;
@@ -30,19 +31,19 @@ public class Anchor extends Circle {
     }
     /**
      *
-     * @param color
      * @param x
      * @param y
      */
-    Anchor(Color color, DoubleProperty x, DoubleProperty y) {
+    Anchor(DoubleProperty x, DoubleProperty y) {
         super(x.get(), y.get(), 10);
         this.x = x;
         this.y = y;
         nbChild = 0;
         depth = 0;
-        setFill(color.deriveColor(1, 1, 1, 0.5));
-        setStroke(color);
-        setStrokeWidth(2);
+        //setFill(color.deriveColor(1, 1, 1, 0.5));
+        setFill(Color.OLIVE);
+        setStroke(Color.BLACK.deriveColor(0, 1, 1, 0.5));
+        setStrokeWidth(1);
         setStrokeType(StrokeType.OUTSIDE);
         levels = new ArrayList<>();
         levels.add(this.x);
@@ -55,10 +56,9 @@ public class Anchor extends Circle {
     {
         nbChild++;
 
-        DoubleProperty endX;
+        DoubleProperty endX = new SimpleDoubleProperty(levels.get(depth).get() + 30);
         DoubleProperty endY   = new SimpleDoubleProperty(getCenterY() + 50);
-        endX = new SimpleDoubleProperty(levels.get(depth).get() + 30);
-        Anchor child = new Anchor(Color.RED, endX, endY);
+        Anchor child = new Anchor(endX, endY);
         Line line = new BoundLine(x, y, endX, endY);
 
         child.depth = depth + 1;
@@ -74,6 +74,7 @@ public class Anchor extends Circle {
         while (_parent.parent != null){ _parent = _parent.parent;}
         children.add(child);
         child.recenterParent();
+        child.toFront();
         return new Group(child, line);
     }
 
@@ -119,52 +120,50 @@ public class Anchor extends Circle {
         }
     }
 
-
-
     // make a node movable by dragging it around with the mouse.
-            private void enableDrag() {
-                final Delta dragDelta = new Delta();
-                setOnMousePressed(new EventHandler<MouseEvent>() {
-                    @Override public void handle(MouseEvent mouseEvent) {
-                        // record a delta distance for the drag and drop operation.
-                        dragDelta.x = getCenterX() - mouseEvent.getX();
-                        dragDelta.y = getCenterY() - mouseEvent.getY();
-                        getScene().setCursor(Cursor.MOVE);
-                    }
-                });
-                setOnMouseReleased(new EventHandler<MouseEvent>() {
-                    @Override public void handle(MouseEvent mouseEvent) {
-                        getScene().setCursor(Cursor.HAND);
-                    }
-                });
-                setOnMouseDragged(new EventHandler<MouseEvent>() {
-                    @Override public void handle(MouseEvent mouseEvent) {
-                        double newX = mouseEvent.getX() + dragDelta.x;
-                        if (newX > 0 && newX < getScene().getWidth()) {
-                            setCenterX(newX);
-                        }
-                        double newY = mouseEvent.getY() + dragDelta.y;
-                        if (newY > 0 && newY < getScene().getHeight()) {
-                            setCenterY(newY);
-                        }
-                    }
-                });
-                setOnMouseEntered(new EventHandler<MouseEvent>() {
-                    @Override public void handle(MouseEvent mouseEvent) {
-                        if (!mouseEvent.isPrimaryButtonDown()) {
-                            getScene().setCursor(Cursor.HAND);
-                        }
-                    }
-                });
-                setOnMouseExited(new EventHandler<MouseEvent>() {
-                    @Override public void handle(MouseEvent mouseEvent) {
-                        if (!mouseEvent.isPrimaryButtonDown()) {
-                            getScene().setCursor(Cursor.DEFAULT);
-                        }
-                    }
-                });
+    private void enableDrag() {
+        final Delta dragDelta = new Delta();
+        setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override public void handle(MouseEvent mouseEvent) {
+                // record a delta distance for the drag and drop operation.
+                dragDelta.x = getCenterX() - mouseEvent.getX();
+                dragDelta.y = getCenterY() - mouseEvent.getY();
+                getScene().setCursor(Cursor.MOVE);
             }
-        private class Delta { double x, y; }
+        });
+        setOnMouseReleased(new EventHandler<MouseEvent>() {
+            @Override public void handle(MouseEvent mouseEvent) {
+                getScene().setCursor(Cursor.HAND);
+            }
+        });
+        setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override public void handle(MouseEvent mouseEvent) {
+                double newX = mouseEvent.getX() + dragDelta.x;
+                if (newX > 0 && newX < getScene().getWidth()) {
+                    setCenterX(newX);
+                }
+                double newY = mouseEvent.getY() + dragDelta.y;
+                if (newY > 0 && newY < getScene().getHeight()) {
+                    setCenterY(newY);
+                }
+            }
+        });
+        setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override public void handle(MouseEvent mouseEvent) {
+                if (!mouseEvent.isPrimaryButtonDown()) {
+                    getScene().setCursor(Cursor.HAND);
+                }
+            }
+        });
+        setOnMouseExited(new EventHandler<MouseEvent>() {
+            @Override public void handle(MouseEvent mouseEvent) {
+                if (!mouseEvent.isPrimaryButtonDown()) {
+                    getScene().setCursor(Cursor.DEFAULT);
+                }
+            }
+        });
+    }
+    private class Delta { double x, y; }
 }
 
 class BoundLine extends Line {
@@ -176,7 +175,7 @@ class BoundLine extends Line {
         setStrokeWidth(2);
         setStroke(Color.BLACK.deriveColor(0, 1, 1, 0.5));
         setStrokeLineCap(StrokeLineCap.BUTT);
-        //getStrokeDashArray().setAll(10.0, 5.0);
+//        getStrokeDashArray().setAll(10.0, 5.0);
         setMouseTransparent(true);
     }
 }
