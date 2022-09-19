6a1e3d142e30a88f548429dcdf77377e7ccf8c8b
Genereux-akotenou <genereux.akotenou@gmail.com>
2022-09-19 11:10:59 +0100

    Feat : Test re-implemented with ParameterizedTest

--- src/test/java/org/uclouvain/visualsearchtree/bridge/DecoderTest.java
+++ src/test/java/org/uclouvain/visualsearchtree/bridge/DecoderTest.java
@@ -3,11 +3,19 @@ package org.uclouvain.visualsearchtree.bridge;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class DecoderTest {
     // data
@@ -23,47 +31,74 @@ class DecoderTest {
         buffer.add((byte) 1);
     }
 
-    @Test
-    void addToBuffer() {
-        byte[] new_data = new byte[] {(byte) 2, (byte) 3, (byte) 4, (byte) 5};
+    @ParameterizedTest
+    @CsvSource({
+            "1, 2, 6, 7",
+            "0, 3, 5, 8",
+    })
+    void addToBuffer(int val1, int val2, int val3, int val4) {
+        byte[] new_data = new byte[] {(byte) val1, (byte) val2, (byte) val3, (byte) val4};
         Decoder.addToBuffer(buffer, new_data);
         assertTrue(buffer.size() > new_data.length);
     }
 
-    @Test
-    void readBuffer_tryRetrieveBytes() {
+    @ParameterizedTest
+    @MethodSource("dataProvider")
+    void readBuffer_tryRetrieveBytes(int val1, int val2, int val3, int val4, int size_to_retrieve, Integer[] data_expected) {
         // add data
-        byte[] new_data = new byte[] {(byte) 2, (byte) 3, (byte) 4, (byte) 5};
+        byte[] new_data = new byte[] {(byte) val1, (byte) val2, (byte) val3, (byte) val4};
         Decoder.addToBuffer(buffer, new_data);
 
         // test tasks
-        byte[] dataToBeRetrieve = new byte[5];
-        byte[] expectedToBeRetrieve = new byte[] {(byte) 0, (byte) 1, (byte) 2, (byte) 3, (byte) 4};
-        boolean status = Decoder.readBuffer(dataToBeRetrieve, buffer, 5);
+        byte[] expectedToBeRetrieve = new byte[data_expected.length];
+        byte[] dataToBeRetrieve = new byte[size_to_retrieve];
+        for (int i = 0; i < data_expected.length; i++) {
+            expectedToBeRetrieve[i] = (byte)data_expected[i].intValue();
+        }
+        boolean status = Decoder.readBuffer(dataToBeRetrieve, buffer, size_to_retrieve);
 
         assertTrue(status);
-        assertArrayEquals(dataToBeRetrieve, expectedToBeRetrieve);
-        assertTrue(buffer.size() == 1);
+        assertArrayEquals(expectedToBeRetrieve, dataToBeRetrieve);
+        assertEquals(6 - size_to_retrieve, buffer.size());
     }
 
-    @Test
-    void readBuffer_tryRetrieveMoreBytesThanItContain() {
-        byte[] dataToBeRetrieve = new byte[10];
+    static Stream<Arguments> dataProvider() {
+        return Stream.of(
+                arguments(3, 4, 5, 6, 3, new Integer[]{0,1,3}),
+                arguments(4, 8, 5, 9, 4, new Integer[]{0,1,4,8})
+        );
+    }
+    @ParameterizedTest
+    @CsvSource({
+            "12",
+            "24",
+            "8",
+    })
+    void readBuffer_tryRetrieveMoreBytesThanItContain(int size) {
+        byte[] dataToBeRetrieve = new byte[size];
         Exception exception = assertThrows(
                 IndexOutOfBoundsException.class, () ->
-                Decoder.readBuffer(dataToBeRetrieve, buffer, 10)
+                Decoder.readBuffer(dataToBeRetrieve, buffer, size)
         );
         assertEquals("Index 0 out of bounds for length 0", exception.getMessage());
     }
 
-    @Test
-    void readBuffer_tryRetrieveNegativeSizeOfBytes() {
-        byte[] dataToBeRetrieve = new byte[1];
+    @ParameterizedTest
+    @CsvSource({
+            "6", "7", "1",
+    })
+    void readBuffer_tryRetrieveNegativeSizeOfBytes(int value) {
+        byte[] dataToBeRetrieve = new byte[value];
         boolean status = Decoder.readBuffer(dataToBeRetrieve, buffer, -1);
         assertTrue(!status);
     }
 
     @Test
+//    @ParameterizedTest
+//    @CsvSource({
+//            "1, 2, 6, 7",
+//            "0, 3, 5, 8",
+//    })
     void byteArrayToInt() {
         byte[] byteData = new byte[] {(byte) 0, (byte) 1, (byte) 2, (byte) 3};
         assertTrue(Decoder.byteArrayToInt(byteData, "BIG_ENDIAN") == 66051);
